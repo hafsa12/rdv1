6f496962b58788c787a7edea3fee44b0c4a58720
hafsa12 <42964475+hafsa12@users.noreply.github.com>
2020-11-23 16:19:34 +0100

    Delete entites.sql

--- entites.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `T_TRADUCTION_LIBELLE` (`ID_TRADUCTION`, `LANG`, `LIBELLE`) VALUES ('20025', 'fr', 'Cour d\'appel de commerce de Casablanca');
-INSERT INTO `T_TRADUCTION_LIBELLE` (`ID_TRADUCTION`, `LANG`, `LIBELLE`) VALUES ('20025', 'ar', 'محكمة الاستئناف التجارية بالدار البيضاء');
